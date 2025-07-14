52aba0623c07bb31a3033f4a8960e4c048ab1fbb
rohan <andriamihaja2007rohan@gmail.com>
2025-07-14 09:29:51 +0100

    first commit

---
+++ include/connexion.php
@@ -0,0 +1,17 @@
+<?php
+function connexion() {  
+    static $bdd = null;
+    if ($bdd === null) {
+        $bdd = mysqli_connect("localhost", "root", "", "");
+        // $bdd = mysqli_connect("ip", "ETU003918", "s7mSG5Zt", "");
+        // $bdd = mysqli_connect("localhost", "ETU004014", "MHV18XnP", "");
+
+        
+        if (!$bdd) {
+            die("Erreur de connexion à la base de données : " . mysqli_connect_error());
+        }
+        
+    }
+    return $bdd;
+}
+?>
---
+++ pages/index.php
@@ -0,0 +1,18 @@
+<?php
+include '../include/function.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
